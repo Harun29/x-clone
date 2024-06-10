d91442c914eee1f501d9274b82925515e30d5e01
Harun29 <ibrahimovic.harun01@gmail.com>
2024-06-10 23:06:35 +0200

    created create account card

--- src/components/cards/CreateAccountCard.jsx
+++ src/components/cards/CreateAccountCard.jsx
@@ -1,20 +1,54 @@
+import '../../styles/cards.css'
+
 const CreateAccountCard = () => {
   return ( 
-    <form>
-      <button className="exit-button"></button>
+    <form className='create-account-card'>
+      <button className="exit-button">X</button>
+
       <div className="card-logo">
-        <img src="" alt="" />
+        <img src="xlogo.png" alt="" />
       </div>
+
+      <span className='create-your-account-span'>Create your account</span>
+
       <div className="name-input-card">
-        <input type="text" />
+        <input type="text" placeholder='Name'/>
         <div className="word-counter-card"></div>
       </div>
-      <div className="email-card-input">
-        <input type="text" />
+
+      <div className="email-input-card">
+        <input type="text" placeholder='Email'/>
         <div className="word-counter-card"></div>
       </div>
-      <span className="date-of-birth">Date of birth</span>
-      <span className="bitrhday-warrning">This will not be shown publicaly. Confirm your age, even if this account is for bussiness, a pet, or something else.</span>
+
+      <div className='birthday-note'>
+        <span className="date-of-birth">Date of birth</span>
+        <span className="birthday-warning">This will not be shown publicaly. Confirm your age, even if this account is for bussiness, a pet, or something else.</span>
+      </div>
+
+      
+      <div className='create-account-birthday'>
+        <select name="month" id="month">
+          <option value="Jan">Jan</option>
+          <option value="Feb">Feb</option>
+          <option value="Mar">Mar</option>
+        </select>
+
+        <select name="day" id="day">
+          <option value="First">1.</option>
+          <option value="Second">2.</option>
+          <option value="Third">3.</option>
+        </select>
+
+        <select name="year" id="year">
+          <option value="2001">2001</option>
+          <option value="2002">2002</option>
+          <option value="2003">2003</option>
+        </select>
+      </div>
+
+      <button className='next-button'>Next</button>
+
     </form>
    );
 }
