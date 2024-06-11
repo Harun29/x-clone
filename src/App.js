6c93329bd34116059a869535f1568a8d04b7bf3e
Harun29 <ibrahimovic.harun01@gmail.com>
2024-06-11 22:56:22 +0200

    created sign in card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import CreateAccountCard from './components/cards/CreateAccountCard';
 import FollowInitialAccountsCard from './components/cards/FollowInitialAccountsCard';
 import PasswordCard from './components/cards/PasswordCard';
 import ProfilePictureCard from './components/cards/ProfilePictureCard';
+import SignInCard from './components/cards/SignInCard';
 import UsernameCard from './components/cards/UsernameCard';
 import VerivicationCard from './components/cards/VerificationCard';
 import SignUpPage from './components/pages/SignUpPage';
@@ -10,12 +11,13 @@ function App() {
   return (
     <div className="App">
       <SignUpPage />
-      {/* <CreateAccountCard />
+      <SignInCard />
+      <CreateAccountCard />
       <VerivicationCard />
       <PasswordCard />
       <ProfilePictureCard />
       <UsernameCard />
-      <FollowInitialAccountsCard /> */}
+      <FollowInitialAccountsCard />
     </div>
   );
 }
---
+++ src/components/cards/SignInCard.jsx
@@ -0,0 +1,57 @@
+import '../../styles/cards.css'
+import '../../styles/pages.css'
+import AppleIcon from "../../icons/AppleIcon";
+import GoogleIcon from "../../icons/GoogleIcon";
+import XIcon from '../../icons/XIcon';
+
+const SignInCard = () => {
+  return ( 
+    <form className="card-form sign-in-card">
+      <div className="card-logo">
+            <img src="xlogo.png" alt="" />
+          </div>
+      <div className='sign-in-card-content-container'>
+        <button className="exit-button">
+          <XIcon />
+        </button>
+
+        <div>
+          
+
+          <span className='card-header-span'>Sign in to X</span>
+        </div>
+
+        <div className='sign-in-top-buttons'>
+          <button className="third-party-signup">
+              <GoogleIcon />
+              Sign in with Google
+          </button>
+          <button className="third-party-signup">
+            <AppleIcon />
+            Sign in with Apple
+          </button>
+        </div>
+        <div className="or-statement-in-signup">
+          <div className="white-line"></div>
+          <span>or</span>
+          <div className="white-line"></div>
+        </div>
+
+        <div className="card-input">
+          <input type="text" placeholder='Email, or username'/>
+          <div className="word-counter-card"></div>
+        </div>
+
+        <div className='sign-in-bottom-buttons'>
+          <button className='card-next-button'>Next</button>
+          <button className='skip-button'>Forgot password?</button>
+        </div>
+
+        <p className='note'>Don't have an account? <a className='link'>Sign up</a></p>
+      </div>
+
+    </form>
+   );
+}
+ 
+export default SignInCard;
